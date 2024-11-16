Fixed normalization bug and changed flow on audio reconstruction.

free_spoken_digit/main_train.py
@@ -42,7 +42,7 @@ def main():
     
     # Set device
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    batch_size = 32
+    batch_size = 16
     set_seed(111)
     data_module = SpectrogramDataModule(
     data_dir="C:/Users/Acer/work/data/free-audio-spectrogram",
@@ -126,7 +126,7 @@ def generate_spectrograms():
 
 if __name__ == '__main__':
     # First create the spectrorgrams, you probably need to do this once.
-    generate_spectrograms()    
+    # generate_spectrograms()    
     # Load history    
     history, test_loader = main()
     # test(test_loader)

free_spoken_digit/spectrogram_data_module.py
@@ -5,7 +5,7 @@ from typing import Optional, Dict, Tuple, List
 import numpy as np
 from sklearn.model_selection import StratifiedShuffleSplit
 import torchvision.transforms as transforms
-
+from visualizations import plot_spectrogram, plot_batch_spectrograms,plot_spectrogram_tensor_or_array
 # Define transform functions outside the class
 def squeeze_tensor(x):
     """Remove extra dimension."""
@@ -26,7 +26,7 @@ class SpectrogramDataModule:
                  data_dir: str,
                  batch_size: int = 32,
                  train_val_split: float = 0.9,
-                 num_workers: int = 4,
+                 num_workers: int = 8,
                  pin_memory: bool = True):
         """
         Args:
@@ -46,7 +46,6 @@ class SpectrogramDataModule:
         self.transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Lambda(squeeze_tensor),
-            transforms.Lambda(normalize_tensor),
             transforms.Lambda(un_squeeze_tensor)
         ])
         
@@ -68,12 +67,10 @@ class SpectrogramDataModule:
         
         def __getitem__(self, idx: int) -> Tuple[torch.Tensor, int]:
             spec_path = self.file_paths[idx]
-            spec = np.load(spec_path)
+            spec = np.load(spec_path)            
             label = self._extract_label(spec_path.stem)
-            
             if self.transform:
                 spec = self.transform(spec)
-                
             return spec, label
         
         @staticmethod
@@ -155,11 +152,21 @@ class SpectrogramDataModule:
             split_ratio=self.train_val_split
         )
         
-        # Print class distribution for verification
         print("\nClass distribution in splits:")
-        print("Training set:", self.get_class_distribution(self.train_dataset))
-        print("Validation set:", self.get_class_distribution(self.val_dataset))
-        print("Test set:", self.get_class_distribution(self.test_dataset))
+        self.print_class_distribution("Training", self.get_class_distribution(self.train_dataset))
+        self.print_class_distribution("Validation", self.get_class_distribution(self.val_dataset))
+        self.print_class_distribution("Test", self.get_class_distribution(self.test_dataset))
+        
+    def print_class_distribution(self,dataset_name, distribution):
+        print("\nClass distribution in splits:")        
+        print(f"\n{dataset_name} set:")
+        print("Class | Count")
+        print("-" * 13)
+        for class_label, count in distribution.items():
+            print(f"{class_label:5d} | {count:5d}")
+        print(f"Total | {sum(distribution.values()):5d}")
+            
+        
     
     def train_dataloader(self) -> DataLoader:
         return DataLoader(

free_spoken_digit/spectrogram_processor.py
@@ -55,6 +55,18 @@ class SpectrogramProcessor:
         # Storage for min max values per audio file. We will use this in reconstruction of audio
         self.min_max_values = {}
         
+        self.inverse_mel_scale_transform = torchaudio.transforms.InverseMelScale(
+            n_stft=self.config.n_fft // 2 + 1,
+            n_mels=self.config.n_mels,
+            sample_rate=self.config.sample_rate
+        )
+
+        self.griffin_lim_transform = torchaudio.transforms.GriffinLim(
+            n_fft=self.config.n_fft,
+            hop_length=self.config.hop_length,
+            n_iter=100
+        )
+        
     def _show_spectrogram(self,S_db):        
         plt.figure(figsize=(12, 8))
         plt.imshow(S_db[0].numpy(), aspect='auto', origin='lower', interpolation='nearest')
@@ -65,31 +77,21 @@ class SpectrogramProcessor:
         plt.tight_layout()
         plt.show()
     
-    def create_audio_from_spectrogram(self, spectrogram: torch.Tensor, min_val: float, max_val: float) -> torch.Tensor:                
-        # First, we need to convert back from dB to power
-        S_reverse = torch.exp(S_db * 0.11512925464970229)  # 10^(x/10) = e^(x * ln(10)/10)
+    def create_audio_from_spectrogram(self, S_db: torch.Tensor, min_val: float, max_val: float) -> torch.Tensor:                
+        # First we have De-normalize the signal. The order in which
+        # we reconstruction the audio is important. 
+        spectrogram_denormalized = S_db * (max_val - min_val) + min_val
+        
+        
+        # Second, we need to convert back from dB to power
+        S_reverse = torch.exp(spectrogram_denormalized * 0.11512925464970229)  # 10^(x/10) = e^(x * ln(10)/10)
 
         # Now we can invert the mel spectrogram
-        inverse_mel_scale = torchaudio.transforms.InverseMelScale(
-            n_stft=n_fft // 2 + 1,
-            n_mels=n_mels,
-            sample_rate=sr
-        )
-
-        griffin_lim = torchaudio.transforms.GriffinLim(
-            n_fft=n_fft,
-            hop_length=hop_length,
-            n_iter=100
-        )
-
-        y_reverse = griffin_lim(inverse_mel_scale(S_reverse))
-
-        # 6. Denormalize the reconstructed audio
-        y_reverse_denormalized = y_reverse * (waveform_max - waveform_min) + waveform_min
-
-        # 7. Enhance volume
+        y_reverse = self.griffin_lim_transform(self.inverse_mel_scale_transform(S_reverse))
+        
+        # Now we can enhance volume
         volume_enhance_factor = 2.0  # Adjust this value to increase or decrease volume
-        y_reverse_enhanced = y_reverse_denormalized * volume_enhance_factor
+        y_reverse_enhanced = y_reverse * volume_enhance_factor
 
         # Clip the enhanced audio to avoid distortion
         y_reverse_enhanced = torch.clamp(y_reverse_enhanced, -1.0, 1.0)
@@ -98,49 +100,49 @@ class SpectrogramProcessor:
             
     def create_spectrogram_from_audio(self, waveform: torch.Tensor, sr: int) -> Tuple[torch.Tensor, float,float]:
         """Process a single audio file to mel spectrogram
-            1- Find min/max for each file and store individually - we will need them at reconstruction
-            2- Do any necessary preprocessing
+            1- Do any necessary preprocessing
             2- Create the mel spectrograms
             3- Convert to decibels
+            4- Get min/max values for reconstruction - then normalize
             4- Save everything  that is needed for reconstruction and model training. 
+            
             
         """
        
-        
-        # Apply Min/Max Normalization
-        waveform_min = torch.min(waveform)
-        waveform_max = torch.max(waveform)        
-        waveform_normalized = (waveform - waveform_min) / (waveform_max - waveform_min)
-
         # Preprocessing steps 
         # Resample if necessary
         if sr != self.config.sample_rate:
             resampler = torchaudio.transforms.Resample(sr, self.config.sample_rate)
-            waveform_normalized = resampler(waveform_normalized)
+            waveform = resampler(waveform)
         
         # Convert to mono if necessary
-        if waveform_normalized.shape[0] > 1:
-            waveform_normalized = torch.mean(waveform_normalized, dim=0, keepdim=True)
+        if waveform.shape[0] > 1:
+            waveform = torch.mean(waveform, dim=0, keepdim=True)
         
         # Exact padding/trimming for precise number of samples
-        if waveform_normalized.shape[1] < self.num_samples:
-            waveform_normalized = torch.nn.functional.pad(
-                waveform_normalized, (0, self.num_samples - waveform_normalized.shape[1])
+        if waveform.shape[1] < self.num_samples:
+            waveform = torch.nn.functional.pad(
+                waveform, (0, self.num_samples - waveform.shape[1])
             )
-        elif waveform_normalized.shape[1] > self.num_samples:
-            waveform_normalized = waveform_normalized[:, :self.num_samples]
+        elif waveform.shape[1] > self.num_samples:
+            waveform = waveform[:, :self.num_samples]
         
         # Create the Mel Spectrograms
-        S = self.mel_spectrogram_transform(waveform_normalized)
+        S = self.mel_spectrogram_transform(waveform)
         # Convert to dB scale
         S_db = torchaudio.transforms.AmplitudeToDB()(S)
         if S_db.shape[1] != self.config.n_mels:
             raise ValueError(f"Expected {self.config.n_mels} mel bins, but got {S_db.shape[1]}")
         if S_db.shape[2] != self.config.n_mels:
             raise ValueError(f"Expected {self.config.n_mels} mel bins, but got {S_db.shape[2]}")
-        #self._show_spectrogram(S_db)
         
-        return S_db, waveform_min, waveform_max
+        # Get min/max, then normalize the spectrogram
+        S_min = torch.min(S_db)
+        S_max = torch.max(S_db)
+        S_normalized = (S_db - S_min) / (S_max - S_min)
+        #self._show_spectrogram(S_normalized)
+        
+        return S_normalized, S_min, S_max
 
     def create_spectrograms_and_save(self, 
                             audio_dir: str, 

free_spoken_digit/visualizations.py
@@ -7,8 +7,42 @@ from pathlib import Path
 from typing import Optional, Union, List
 import seaborn as sns
 
+
+def plot_spectrogram_tensor_or_array(
+    spec: Union[torch.Tensor, np.ndarray],
+    sr : int = 22050,
+    hop_length : int = 128,
+    figsize: tuple = (10, 4),
+    save_path: Optional[str] = None
+) -> None:
+    """Plot a single spectrogram from Tensor or numpy array."""
+    if isinstance(spec, torch.Tensor):
+        spec = spec.numpy()
+        
+    plt.figure(figsize=figsize)
+    librosa.display.specshow(
+        spec[0] if spec.ndim == 3 else spec,
+        y_axis='mel',
+        x_axis='time',
+        sr=sr,  
+        hop_length=hop_length 
+    )
+    plt.colorbar(format='%+2.0f dB')
+    if save_path:
+        plt.title(f'Mel Spectrogram: {Path(spec_path).stem}')
+    
+    if save_path:
+        plt.savefig(save_path, dpi=300, bbox_inches='tight')
+        plt.close()
+    else:
+        plt.draw()
+        plt.pause(0.01)  # Short pause to update the plot        
+        plt.show()
+
 def plot_spectrogram(
     spec_path: Union[str, Path],
+    sr : int = 22050,
+    hop_length : int = 128,
     figsize: tuple = (10, 4),
     save_path: Optional[str] = None
 ) -> None:
@@ -23,8 +57,8 @@ def plot_spectrogram(
         spec[0] if spec.ndim == 3 else spec,
         y_axis='mel',
         x_axis='time',
-        sr=22050,  # Default sample rate
-        hop_length=256  # Default hop length
+        sr=sr,  
+        hop_length=hop_length 
     )
     plt.colorbar(format='%+2.0f dB')
     plt.title(f'Mel Spectrogram: {Path(spec_path).stem}')
@@ -33,11 +67,15 @@ def plot_spectrogram(
         plt.savefig(save_path, dpi=300, bbox_inches='tight')
         plt.close()
     else:
+        plt.draw()
+        plt.pause(0.01)  # Short pause to update the plot        
         plt.show()
 
 def plot_batch_spectrograms(
     specs: Union[torch.Tensor, np.ndarray],
     num_examples: int = 4,
+    sr = 22050,
+    hop_length = 128,
     figsize: tuple = (15, 3),
     save_path: Optional[str] = None,
     titles: Optional[List[str]] = None
@@ -54,12 +92,18 @@ def plot_batch_spectrograms(
     
     for i in range(num_examples):
         spec = specs[i]
+        # Ensure spec is 2D by squeezing or selecting the appropriate slice
+        if spec.ndim == 3:
+            spec = np.squeeze(spec)  # Remove singleton dimensions
+        if spec.ndim != 2:
+            raise ValueError(f"Unexpected spectrogram shape: {spec.shape}")
+        
         img = librosa.display.specshow(
             spec,
             y_axis='mel',
             x_axis='time',
-            sr=22050,  # Default sample rate
-            hop_length=256,  # Default hop length
+            sr=sr, 
+            hop_length=hop_length,
             ax=axes[i]
         )
         title = f'Sample {i+1}' if titles is None else titles[i]
@@ -72,6 +116,8 @@ def plot_batch_spectrograms(
         plt.savefig(save_path, dpi=300, bbox_inches='tight')
         plt.close()
     else:
+        plt.draw()
+        plt.pause(0.01)  # Short pause to update the plot
         plt.show()
 
 def compare_spectrograms(

training/trainer.py
@@ -9,6 +9,7 @@ from collections import defaultdict
 from tqdm import tqdm
 import json
 from common.visualizations import visualize_reconstructions
+from visualizations import plot_batch_spectrograms
 
 
     
@@ -74,7 +75,7 @@ class VAETrainer:
             recon_x.view(-1), 
             x.view(-1)
         )
-        
+                
         # KL divergence loss
         kl_loss = -0.5 * torch.sum(
             1 + log_var - mu.pow(2) - log_var.exp()
@@ -118,8 +119,7 @@ class VAETrainer:
                 
                 # Forward pass - noisy_x will be none if VAE was constructed with noise_factor = 0.0
                 recon_x, mu, log_var, noisey_x = self.model(data)
-                
-                
+                                
                 # Compute loss
                 loss, metrics = self.compute_loss(recon_x, data, mu, log_var)
                 
@@ -260,6 +260,7 @@ class VAETrainer:
             # Optional: Visualize reconstructions every N epochs
             if epoch % 5 == 0 and self.reconstruction_save_dir:
                 visualize_reconstructions(self.model, val_loader, self.device, display=False,save_path=self.reconstruction_save_dir)
+            visualize_reconstructions(self.model, val_loader, self.device, display=False,save_path=self.reconstruction_save_dir)    
         
         
         return dict(history)